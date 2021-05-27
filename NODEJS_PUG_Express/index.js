9266948c78ad3029fc4e79fb25951958c620e156
harpreet-nodejs2021 <harpreet.singh.avionics@gmail.com>
2021-05-26 21:08:26 -0500

    body parser files

--- NODEJS_PUG_Express/index.js
+++ NODEJS_PUG_Express/index.js
@@ -6,6 +6,13 @@ const app = express();
 
 const port = 3000; // Local Host random port #
 
+// Middleware - This code get executed before we hit the route.
+
+const bodyParser = require('body-parser');
+const urlEncodedParser = bodyParser.urlencoded({extended:false}); // This piece will be used to access the Post Request's body which is otherwise not part of the URL.
+
+// "bodyParser" - Deprecated
+
 const superheroes = [
     { id:1, name: 'SPIDER-MAN'},
     { id:2, name: 'CAPTAIN MARVEL' },
@@ -38,7 +45,17 @@ app.get('/superheroes/:id', function(req,res){
     selectedSuperHero = selectedSuperHeroArray[0];
     res.render('superhero', { superhero: selectedSuperHero }); // rendering superhero.pug file
 });
-  
+
+app.post('/superheroes', urlEncodedParser, function(req,res){
+    const newId = superheroes[superheroes.length-1].id + 1; // Accessing Superheroes Array and adding 1 to the id of the last element of the array.
+    const newSuperHero = {
+        id: newId, // to generate a New Id
+        name: req.body.superhero // Use same name as of the element defined in textbox of file "May26_createNewSuperhero.pug"
+    }
+    superheroes.push(newSuperHero);
+    res.redirect('/'); // New Superhero object gets created/ pushed to the page and page gets refreshed.
+}); 
+
 // Another way to write Function Below using Arrow function (=>)
 
 app.listen(port, () => {
